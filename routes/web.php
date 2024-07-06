38cd5fe7353cb3b3a74a07a096cd44fe5164c58f
EduardRo <eduardstancu@yahoo.com>
2024-07-06 14:16:17 +0300

    create views

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+    
+});
+
+Route::get('/matrix', function () {
+    return ['1'=> 'first', '2'=> 'second', '3'=> 'third'];
+});
+
+Route::get('about', function() {
+    return view('about');
+});
+
+Route::get('contact', function() {
+    return view('contact');
 });
