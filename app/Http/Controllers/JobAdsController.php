79c230ed94170c1fda60d5f3b37cac768c531760
EduardRo <eduardstancu@yahoo.com>
2024-07-11 23:08:59 +0300

    JobAdsController added

---
+++ app/Http/Controllers/JobAdsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobAds;
+use Illuminate\Http\Request;
+
+class JobAdsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JobAds $jobAds)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JobAds $jobAds)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, JobAds $jobAds)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JobAds $jobAds)
+    {
+        //
+    }
+}
